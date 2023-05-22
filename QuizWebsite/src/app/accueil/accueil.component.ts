ecf6462c7496a61c252c2b39fc6a04cc7649dabd
Elyes <elyess.laiche@gmail.com>
2023-05-23 01:39:49 +0200

    Site Web Ok manque que le css

--- QuizWebsite/src/app/accueil/accueil.component.ts
+++ QuizWebsite/src/app/accueil/accueil.component.ts
@@ -1,5 +1,6 @@
-import { Component , Input } from '@angular/core';
-import {LoginService, UserLogged} from "../services/login.service";
+import { Component, OnInit, Input } from '@angular/core';
+import { LoginService } from "../services/login.service";
+
 
 @Component({
   selector: 'app-accueil',
@@ -7,13 +8,15 @@ import {LoginService, UserLogged} from "../services/login.service";
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent {
-  constructor(private ls: LoginService){ }
+  constructor(private ls: LoginService) { }
 
   // Inherit attributes from the parent component
   @Input() dashboardIndex = 0;
   @Input() toolbar = 'hidden';
   @Input() vizUrl = 'https://public.tableau.com/views/GarbageFE/Evolutiondesdechets?:language=fr-FR&publish=yes&:display_count=n&:origin=viz_share_link';
 
-  // Dashboard properties
   public VizIndex = 'Tableau-Viz-' + this.dashboardIndex;
+
+  ngOnInit(): void {
+  }
 }
